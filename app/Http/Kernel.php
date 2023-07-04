6fb5506646af1f0f6305edc063b83ee91169ab69
asrakt <ashraktamin678@gmail.com>
2023-07-04 16:01:15 +0200

    project and unit update and delete

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,9 +39,12 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+                'throttle:api',
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                'cors',              
+        
+            //\Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
         ],
     ];
 
@@ -64,5 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class,
+
     ];
 }
