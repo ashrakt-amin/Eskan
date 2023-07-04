12794ed06d0d91ecea99d786fe904a6e7b535f03
asrakt <ashraktamin678@gmail.com>
2023-07-04 11:28:12 +0200

    filter unit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\unitsTypeSeeder;
 
 class DatabaseSeeder extends Seeder
 {
