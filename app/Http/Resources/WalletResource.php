efe28a8792a2fa5dd855e62b1c01cedca6574d68
Ashraqt <ashraktamin678@gmail.com>
2023-08-23 06:37:16 -0700

    wallet

--- app/Http/Resources/WalletResource.php
+++ app/Http/Resources/WalletResource.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class WalletResource extends JsonResource
 {
-       $formattedCreatedAt = Carbon::parse($this->created_at)->format('Y-m-d H:i:s');
+      
     public function toArray(Request $request): array
     {
+ $formattedCreatedAt = Carbon::parse($this->created_at)->format('Y-m-d H:i:s');
         return [
             'id'          => $this->id,
             'name'        => $this->name,
