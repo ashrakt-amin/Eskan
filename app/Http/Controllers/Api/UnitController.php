5f8bb02ca288cc37e6749b431cdf25fdf3d175d7
Ashraqt <ashraktamin678@gmail.com>
2023-09-16 04:31:12 -0700

    contact us

--- app/Http/Controllers/Api/UnitController.php
+++ app/Http/Controllers/Api/UnitController.php
@@ -32,12 +32,10 @@ class UnitController extends Controller
     public function store(UnitRequest $request)
     {
         $data = $this->Repository->store($request->validated());
-        return $this->sendResponse($data, "    ", 200);
-
         if (isset($data->errorInfo)) {
             return $this->sendError($data->errorInfo, 'error', 404);
         } else {
-            return $this->sendResponse('', "تم تسجيل وحده جديده بنجاح", 200);
+            return $this->sendResponse($data, "تم تسجيل وحده جديده بنجاح", 200);
         }
     }
 
