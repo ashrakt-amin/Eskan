e13dc9fd0c96c87be541f50bbbab7c0314a4d3d9
unknown <ashraktamin678@gmail.com>
2023-10-22 11:39:37 +0300

    samira account

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,12 +12,37 @@ class UserSeeder extends Seeder
    
     public function run(): void
     {
+
         DB::table('users')->delete();
-        User::create([
+
+        $users = [
+        [
             'name'     => 'admin',
             'phone'    => '01025248183',
-            'password' => bcrypt('marwa123')
-        ]);
+            'password' => bcrypt('marwa123')   
+           ],
+        [
+            'name'     => 'admin',
+            'phone'    => '01099388290',
+            'password' => bcrypt('1234566'),
+            ]
+        ];
+
+        foreach($users as $user){
+            User::create([
+                'name'     => $user['name'],
+                'phone'    => $user['phone'],
+                'password' => $user['password'],
+            ]);
+        }
+
+        // DB::table('users')->delete();
+        // User::create([
+        //     'name'     => 'admin',
+        //     'phone'    => '01025248183',
+        //     'password' => bcrypt('marwa123')
+        // ]);
+
     }
     
 }
