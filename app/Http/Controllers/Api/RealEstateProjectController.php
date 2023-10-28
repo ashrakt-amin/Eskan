34e98774710ddf859164e4a457a019b5c24a8526
unknown <ashraktamin678@gmail.com>
2023-10-28 16:47:23 +0200

    card and show

--- app/Http/Controllers/Api/RealEstateProjectController.php
+++ app/Http/Controllers/Api/RealEstateProjectController.php
@@ -12,120 +12,112 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Resources\Project\projectWallet;
 use App\Http\Traits\ResponseTrait as TraitResponseTrait;
 use App\Http\Traits\ImageProccessingTrait as TraitImageProccessingTrait;
+use App\Http\Resources\Project\projectWalletShow;
 
 
 
 class RealEstateProjectController extends Controller
 {
-use TraitImageProccessingTrait , TraitResponseTrait ;
+    use TraitImageProccessingTrait, TraitResponseTrait;
 
-  
+
     public function index()
     {
         $data = RealEstateProject::all();
-        return $this->sendResponse(projectWallet::collection($data) , " " ,200);
+        return $this->sendResponse(projectWallet::collection($data), " ", 200);
     }
 
-  
+
     public function store(PortfolioProjectRequest $request)
     {
         DB::beginTransaction();
         try {
             $data = new RealEstateProject();
-            $img = $this->aspectForResize($request->img , RealEstateProject::IMAGE_PATH, 500, 600);
+            $img = $this->aspectForResize($request->img, RealEstateProject::IMAGE_PATH, 500, 600);
             $project = $data->create([
                 'name'    =>  $request->name,
                 'img'     =>  $img,
                 'address' =>  $request->address,
                 'resale'  =>  $request->resale,
-                'link'    =>  $request->link == null ? null : $request->link ,
+                'link'    =>  $request->link == null ? null : $request->link,
                 'description' => $request->description,
                 'detalis'  =>  $request->detalis,
                 'features' =>  $request->features == null ? null : $request->features,
-            ]);                
+            ]);
 
-            if($request->file){
-                foreach ($request->file as $file){
+            if ($request->file) {
+                foreach ($request->file as $file) {
                     $fileName = Str::random(8) . '_' . $file->getClientOriginalName();
                     $file_title = $file->getClientOriginalName();
                     $fileWithoutExtension = pathinfo($file_title, PATHINFO_FILENAME);
-                    
+
                     $project->files()->create([
                         'file' => $fileName,
                         'name' => $fileWithoutExtension,
                     ]);
-                    $file->storeAs(Projectfile::File_PATH, $fileName, 'public');                                  
+                    $file->storeAs(Projectfile::File_PATH, $fileName, 'public');
                 }
             }
             DB::commit();
-            return $this->sendResponse($project, "تم الحفظ" ,200);
-            
+            return $this->sendResponse($project, "تم الحفظ", 200);
         } catch (\Exception $e) {
-            
-            return $this->sendError($e->getMessage(), "error " ,404);
 
+            return $this->sendError($e->getMessage(), "error ", 404);
         }
-
     }
 
- 
+
     public function show($id)
     {
-        return $this->sendResponse(new projectWallet(RealEstateProject::findOrFail($id)) , " " ,200);
+        return $this->sendResponse(new projectWalletShow(RealEstateProject::findOrFail($id)), " ", 200);
     }
 
-    public function updateProject(Request $request,$id)
+    public function updateProject(Request $request, $id)
     {
         $data = RealEstateProject::findOrFail($id);
-        if(isset($request->img)){
-            $this->deleteImage(RealEstateProject::IMAGE_PATH , $data->img);
-            $img = $this->aspectForResize($request->img , RealEstateProject::IMAGE_PATH, 500, 600);
+        if (isset($request->img)) {
+            $this->deleteImage(RealEstateProject::IMAGE_PATH, $data->img);
+            $img = $this->aspectForResize($request->img, RealEstateProject::IMAGE_PATH, 500, 600);
             $data->update([
-                'img' => $img 
+                'img' => $img
             ]);
-
-        }else{
+        } else {
             $data->update($request->all());
         }
-        return $this->sendResponse($data, "تم التعديل" ,200);
-
-        
+        return $this->sendResponse($data, "تم التعديل", 200);
     }
 
     public function destroy($id)
     {
         $data = RealEstateProject::findOrFail($id);
-        $this->deleteImage(RealEstateProject::IMAGE_PATH , $data->img);
-        $files = $data->files ;
-          if($files){
-            foreach($files as $file){
-                $this->deleteImage( projectFile::File_PATH , $file->file);
+        $this->deleteImage(RealEstateProject::IMAGE_PATH, $data->img);
+        $files = $data->files;
+        if ($files) {
+            foreach ($files as $file) {
+                $this->deleteImage(projectFile::File_PATH, $file->file);
             }
-            }
+        }
         $data->delete();
-        return $this->sendResponse("success", "تم الحذف" ,200);
-
+        return $this->sendResponse("success", "تم الحذف", 200);
     }
 
-    
+
     public function addFile(Request $request)
     {
         $data = RealEstateProject::findOrFail($request->id);
-        if($request->file){
-            foreach ($request->file as $file){
+        if ($request->file) {
+            foreach ($request->file as $file) {
                 $fileName = Str::random(8) . '_' . $file->getClientOriginalName();
                 $file_title = $file->getClientOriginalName();
-                $fileWithoutExtension = pathinfo($file_title, PATHINFO_FILENAME);               
+                $fileWithoutExtension = pathinfo($file_title, PATHINFO_FILENAME);
                 $data->files()->create([
                     'file' => $fileName,
                     'name' => $fileWithoutExtension,
                 ]);
-                $file->storeAs(Projectfile::File_PATH, $fileName, 'public');                                  
+                $file->storeAs(Projectfile::File_PATH, $fileName, 'public');
             }
         }
-            return $this->sendResponse($data, "تم الحفظ" ,200);
-       
-
+        return $this->sendResponse($data, "تم الحفظ", 200);
     }
 
 
@@ -134,11 +126,8 @@ use TraitImageProccessingTrait , TraitResponseTrait ;
     public function destroyFile($id)
     {
         $data = projectFile::findOrFail($id);
-        $this->deleteImage(projectFile::File_PATH , $data->file);
+        $this->deleteImage(projectFile::File_PATH, $data->file);
         $data->delete();
-        return $this->sendResponse("success", "تم الحذف" , 200);
-
+        return $this->sendResponse("success", "تم الحذف", 200);
     }
-
-    
 }
--- app/Http/Resources/Project/projectWalletShow.php
+++ app/Http/Resources/Project/projectWalletShow.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Project;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class projectWallet extends JsonResource
+class projectWalletShow extends JsonResource
 {
     public function toArray(Request $request): array
     {
@@ -16,8 +16,13 @@ class projectWallet extends JsonResource
             'img'             => $this->path,
             'address'         => $this->address,
             'resale'          => $this->resale,
-            'detalis'         => $this->detalis
-            
+            'link'            => $this->link,
+            'description'     => $this->description,
+            'detalis'         => $this->detalis,
+            'features'        => $this->features,
+            'files'           => $this->files == null ? "null" :projectFile::collection($this->files),
+
+
         ];  
       }
 }
