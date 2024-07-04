309850d52983589a94a71fae86aa1424e8874d18
ashrakt <ashraktamin678@gmail.com>
2024-07-04 05:11:58 -0700

    off seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,8 +8,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(UserSeeder::class);
-        $this->call(LevelSeeder::class);
-        $this->call(unitsTypeSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(LevelSeeder::class);
+        // $this->call(unitsTypeSeeder::class);
     }
 }
