d7cefd76261312c70e6c0438aed75959f494584b
ashrakt <ashraktamin678@gmail.com>
2024-09-19 08:22:10 -0700

    uniqe names

--- app/Http/Resources/ImagenameResource.php
+++ app/Http/Resources/ImagenameResource.php
@@ -11,7 +11,6 @@ class ImagenameResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'        => $this->id,
             'name'      => $this->name,
         ];    
 
